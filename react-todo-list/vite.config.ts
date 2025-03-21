32c3029925db090fce85f9a801b297b546acb1e8
chuxin <15115359055@139.com>
2025-03-21 16:52:47 +0800

    chore: 更新vite配置中的API代理路径
    修改了vite.config.ts中的代理配置，将API请求的目标路径调整为包含`/api`，并移除了路径重写规则，以简化代理配置

--- react-todo-list/vite.config.ts
+++ react-todo-list/vite.config.ts
@@ -6,9 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:9000',
+        target: 'http://127.0.0.1:9000/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
