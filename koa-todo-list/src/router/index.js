3b28d99889f4f50c924445ee636d352b0ee67593
chuxin <15115359055@139.com>
2025-05-15 17:10:36 +0800

    feat(router): 添加增删改查及分页查询的路由接口
    新增了POST、DELETE、PUT和GET方法的路由接口，分别用于处理增加、删除、修改、查询全部和查询分页的操作，以支持基本的CRUD功能。

--- koa-todo-list/src/router/index.js
+++ koa-todo-list/src/router/index.js
@@ -3,16 +3,28 @@ const KoaRouter = require('koa-router');
 function initRouter(app) {
   const router = new KoaRouter();
 
-  router.get('/index', async (ctx, next) => {
-    ctx.body = 'hello koa2';
+  // 增加
+  router.post('/add', async (ctx) => {
+    ctx.body = '增加';
   });
 
-  // 增加
-
   // 删除
+  router.delete('/delete', async (ctx) => {
+    ctx.body = '删除';
+  });
+
   // 修改
+  router.put('/update', async (ctx) => {
+    ctx.body = '修改';
+  })
   // 查询全部
+  router.get('/query', async (ctx) => {
+    ctx.body = '查询全部';
+  })
   // 查询分页
+  router.get('/queryPage', async (ctx) => {
+    ctx.body = '查询分页';
+  })
 
   app.use(router.routes())
 }
